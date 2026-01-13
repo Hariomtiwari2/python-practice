43ad4e29f892db672c0fb8d6d4e9890712e80ae8
Hariom Tiwari <hariomtiwari20004@gmail.com>
2026-01-13 14:14:41 +0530

    Add if-else example fot age based voting

--- basics/hello.py
+++ basics/hello.py
@@ -1,1 +1,10 @@
 print("hello from python + Github")
+
+# if else 
+age = int(input("Enter your age: "))
+if(age > 18):
+    print("Vote")
+elif(age == 18):
+    print("apply for voter id card")
+else:
+    print("you can't vote")
