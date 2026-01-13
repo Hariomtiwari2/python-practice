1ad3b9516543f977a54db03e2035d609aed728be
Hariom Tiwari <hariomtiwari20004@gmail.com>
2026-01-13 18:53:00 +0530

    while loop in the code

--- basics/hello.py
+++ basics/hello.py
@@ -1,10 +1,20 @@
 print("hello from python + Github")
 
 # if else 
-age = int(input("Enter your age: "))
-if(age > 18):
-    print("Vote")
-elif(age == 18):
-    print("apply for voter id card")
-else:
-    print("you can't vote")
+# age = int(input("Enter your age: "))
+# if(age > 18):
+#     print("Vote")
+# elif(age == 18):
+#     print("apply for voter id card")
+# else:
+#     print("you can't vote")
+
+# while loop    
+i=0
+while(i < 10) :
+    if(i % 2 == 0):
+        print(i," is an even number")
+    else:
+        print(i, " is an Odd number")
+    
+    i=i+1
