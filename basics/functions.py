7f4be4f88fdd3273e17be0322df5f5aacf9eaa99
Hariom Tiwari <hariomtiwari20004@gmail.com>
2026-01-14 22:36:02 +0530

    linear search solved in python

--- basics/functions.py
+++ basics/functions.py
@@ -1,11 +1,26 @@
 # here we will learn about python functions
 
 # function to calculate mean
-def meanCal(a , b):
-    ans  = (a+b)/2
-    print(ans)
+# def meanCal(a , b):
+#     ans  = (a+b)/2
+#     print(ans)
 
-a  = int(input("Enter value of a: "))
-b =  int(input("Enter value of b: "))
+# a  = int(input("Enter value of a: "))
+# b =  int(input("Enter value of b: "))
 
-meanCal(a,b)
+# meanCal(a,b)
+
+arr = [1,2,3,4,5]
+x= int(input("X: "))
+def   linearSearch(arr , x):
+    for i in range(len(arr)):
+        if(arr[i] == x):
+           
+           return i
+    return -1
+        
+ans = linearSearch(arr , x)
+if(ans != -1):
+    print("element is at index: ",ans)
+else:
+    print("element not found")
