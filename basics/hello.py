35347e2ff335980d34776e45a20b8aabd11a2c66
Hariom Tiwari <hariomtiwari20004@gmail.com>
2026-01-14 17:56:25 +0530

    hand's on on break and continue

--- basics/hello.py
+++ basics/hello.py
@@ -10,11 +10,27 @@ print("hello from python + Github")
 #     print("you can't vote")
 
 # while loop    
-i=0
-while(i < 10) :
-    if(i % 2 == 0):
-        print(i," is an even number")
-    else:
-        print(i, " is an Odd number")
+# i=0
+# while(i < 10) :
+#     if(i % 2 == 0):
+#         print(i," is an even number")
+#     else:
+#         print(i, " is an Odd number")
     
-    i=i+1
+#     i=i+1
+
+# break & Continue  statement
+# Break: break statement will end the entire loop if condition met
+
+for i in range(10):
+    print(i)
+    if(i == 5):
+        break
+
+#continue: continue statement skip the particular iteration
+
+for i in range(10):
+    if(i==5):
+        print("5 will not be printed")
+        continue
+    print(i)
