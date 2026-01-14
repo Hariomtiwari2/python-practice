40ec45481706eda2bd42bf3b5bdfe7bdd9355257
Hariom Tiwari <hariomtiwari20004@gmail.com>
2026-01-14 21:51:51 +0530

    made function to calculate avg

---
+++ basics/functions.py
@@ -0,0 +1,11 @@
+# here we will learn about python functions
+
+# function to calculate mean
+def meanCal(a , b):
+    ans  = (a+b)/2
+    print(ans)
+
+a  = int(input("Enter value of a: "))
+b =  int(input("Enter value of b: "))
+
+meanCal(a,b)
