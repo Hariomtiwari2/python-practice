5c1a6ea7374d81b2e0efd7722e921aeb6b931332
Hariom Tiwari <hariomtiwari20004@gmail.com>
2026-01-16 23:57:38 +0530

    practice of some list methods

--- basics/data_structures.py
+++ basics/data_structures.py
@@ -1,19 +1,39 @@
 # list data structure
 # list of 1st 10 even numbers
-even = []
-i=1
-while(i<40):
-    if(i%2 == 0):
-        even.append(i)
-    i= i+1
+# even = []
+# i=1
+# while(i<40):
+#     if(i%2 == 0):
+#         even.append(i)
+#     i= i+1
 
-    if(len(even) == 10):
-        break
+#     if(len(even) == 10):
+#         break
 
-# print(even)
-x=0
-for i in range(1 , 11 ,1):
-    print(i , " ",even[x])
-    x = x+1
+# # print(even)
+# x=0
+# for i in range(1 , 11 ,1):
+#     print(i , " ",even[x])
+#     x = x+1
 
-even.append(100)
+# even.append(100)
+
+
+# list methods
+l = [1,3,54,2,5,6,77]
+
+# #sorting 
+# print(l)
+# print("Sorted list in ascedning order")
+# print(l.sort)
+# print(l)
+# print("Sorted list in descedning order")
+# print(l.sort(reverse=True))
+# print(l)
+
+# print reverse of list
+l.reverse
+print(l)
+
+# to get index of particular element
+print(l.index(77))
