c0b154867685a790149df77eedaf9059952065de
Hariom Tiwari <hariomtiwari20004@gmail.com>
2026-01-19 19:15:21 +0530

    fibonacci code practice

--- basics/Recursion.py
+++ basics/Recursion.py
@@ -4,10 +4,21 @@
 
 #Factorial Function
 
-def factorial(n):
-    if(n == 1 or n == 0):
-        return 1
-    else:
-        return (n*factorial(n-1))
+# def factorial(n):
+#     if(n == 1 or n == 0):
+#         return 1
+#     else:
+#         return (n*factorial(n-1))
     
-print("Factorial of 5: ", factorial(5))
+# print("Factorial of 5: ", factorial(5))
+
+#Fibonacci series
+def fibo(n):
+    if(n == 1):
+        return 1
+    if(n == 0):
+        return 0
+    
+    return (fibo(n-1) + fibo(n-2))
+n = int(input("Enter the number: "))
+print("Fibonaci of ",n,": " ,fibo(n))
