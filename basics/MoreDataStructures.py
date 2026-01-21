cfb444f2184ed1423bb79ea1975bdffd44fc6cef
Hariom Tiwari <hariomtiwari20004@gmail.com>
2026-01-22 00:35:22 +0530

    learning Dictionaries today

--- basics/MoreDataStructures.py
+++ basics/MoreDataStructures.py
@@ -11,18 +11,38 @@
 # for value in s:
 #     print(type(value) , value)
 
-# # harry = set()
+# harry = set()
 # print(type(harry))
 
 # operations on set
 #  1 union op
-s1 = {1,2,3,4}
-s2 = {4,5,6,1}
+# s1 = {1,2,3,4}
+# s2 = {4,5,6,1}
 
-# for i in s1.union(s2):
+# # for i in s1.union(s2):
+# #     print(i)
+# # print(s1.union(s2))
+
+# # intersaction
+# for i in s1.intersection(s2):
 #     print(i)
-# print(s1.union(s2))
 
-# intersaction
-for i in s1.intersection(s2):
-    print(i)
+
+#Dictionaries 
+# dic are ordered
+# dict have key vale pair, seprated by :
+# element in dict are seprated by , 
+
+dict ={"Hariom" : "Human" ,
+       "Spoon" : "Object",
+       1 : 1  
+       }
+print("print entire dict")
+print(dict)
+print("Printing just keys of dict")
+for key in dict.keys():
+    print(dict[key])
+
+print("printing keys values in the dict")
+for key , value in dict.items():
+    print(f"the value of {key} is {value}")
